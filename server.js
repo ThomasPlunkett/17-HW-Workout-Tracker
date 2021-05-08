0b4761a2a92d24d65042204a7a7bd50e041b7369
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-07 23:06:09 -0400

    modified server.js to connect to the mongodb library using mongoose

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = 3001;
 
-const app =express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// connect to the monogdb library using mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on PORT ${PORT}`);
+});
