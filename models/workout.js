1cfcf6cc3d555948ec3b46fbe65087253e433193
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-07 23:51:55 -0400

    modified workouts.js with corrections from mongoose documentation for default function

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: {},
+    day: {
+        type: Date
+        default: Date.now
+    },
     exercises: [
         {
             type: {
